92ef07221bcd27a4776d703e5d6bff41df80e48c
EXPERT <saievmuhammad@mail.ru>
2022-07-27 19:08:09 +0300

    Init spring boot project

---
+++ src/main/java/ru/saiev/epictest/EpicTestApplication.java
@@ -0,0 +1,13 @@
+package ru.saiev.epictest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpicTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/saiev/epictest/EpicTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.saiev.epictest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
