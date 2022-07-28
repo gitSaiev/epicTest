1ad7e73e7c5d9d25a9b2dd3eb55f0638e308bf93
EXPERT <saievmuhammad@mail.ru>
2022-07-28 12:22:55 +0300

    configure Http.Status and exceptions

--- src/main/java/ru/saiev/epictest/dto/CounterDto.java
+++ src/main/java/ru/saiev/epictest/dto/CounterDto.java
@@ -5,12 +5,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
 @Getter
 public class CounterDto {
 
+    @NotNull
     private Integer counterId;
+
     private int incrementCount;
 }
--- src/main/java/ru/saiev/epictest/services/impl/CountServiceImpl.java
+++ src/main/java/ru/saiev/epictest/services/impl/CountServiceImpl.java
@@ -1,7 +1,6 @@
 package ru.saiev.epictest.services.impl;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.saiev.epictest.dto.CounterDto;
 import ru.saiev.epictest.entities.Count;
@@ -13,18 +12,19 @@ import ru.saiev.epictest.services.ValidationService;
 @Service
 @RequiredArgsConstructor
 public class CountServiceImpl implements CountService {
-    private final String MESSAGE = "count not found";
 
     private final CountRepository countRepository;
     private final ValidationService validationService;
 
     public Count getCountById(Integer id) {
-        return countRepository.findById(id).orElseThrow(() -> new CountNotFoundException(MESSAGE));
+        return countRepository.findById(id).orElseThrow(() -> new CountNotFoundException("Count not found!"));
     }
 
     public Count incrementCountById(CounterDto counterDto) {
         validationService.validateIncrementValue(counterDto.getIncrementCount());
-
+        if (getCountById(counterDto.getCounterId()) == null) {
+            throw new CountNotFoundException("Count not found!");
+        }
         Count count = getCountById(counterDto.getCounterId());
         count.setValue(count.getValue() + counterDto.getIncrementCount());
         return countRepository.save(count);
